32308caba8e89fc7027ba14ffa69e547af9a6467
Michael <mike@autom.io>
2018-04-16 18:41:43 +0300

    numbered screens to fix workspace ordering
    - workspace:screen format makes sure the workspaces show in the correct order
    - added pass-through mode to help with conflicts
    - set screen mode 1 on startup/reset so bindings work straight away
    - added compiled config for quick use

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # i3config
+
+#### Generate config with:
+```
+sed '/^[[:space:]]*# config.default_keys/r config.default_keys' config.base > config
+```
--- i3workspace.pl
+++ i3workspace.pl
@@ -1,40 +1,37 @@
 #!/usr/bin/env perl
-
 use strict;
 use warnings;
-
 use POSIX;
-
 use AnyEvent;
 use AnyEvent::I3 qw(:all);
-use Data::Dumper;
-
-my $WORKSPACES = 10;
 
 my $i3 = i3();
 
 $i3->connect->recv or die "Could not connect to i3: $!";
+$i3->message(TYPE_COMMAND, "mode 1");
 
 my %callbacks = (
-    workspace => sub {
-        my $e = shift;
-	my $mode = substr $e->{'current'}->{'name'}, 0, 1;
+  workspace => sub {
+    my $e = shift;
+    my $mode = substr $e->{'current'}->{'name'}, -1;
 
-	$i3->message(TYPE_COMMAND, "mode ${mode}");
-    },
-    _error => sub {
-        my $msg = shift;
+    $i3->message(TYPE_COMMAND, "mode ${mode}");
+  },
+  _error => sub {
+    my $msg = shift;
 
-        unless ($msg eq 'Unexpected end-of-file') {
-            print STDERR "An error occured: $msg";
-            exit 1;
-        }
-
-        exit 0;
+    unless ($msg eq 'Unexpected end-of-file') {
+      print STDERR "An error occured: $msg";
+      exit 1;
     }
+
+    exit 0;
+  }
 );
 
 $i3->subscribe(\%callbacks)->recv->{success}
-    or die "Could not subscribe to the events: $!";
+  or die "Could not subscribe to the events: $!";
+
+
 
 AE::cv->recv;
